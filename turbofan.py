07a288d82edf9823f4354b89e9afabe00973029a
maksym.valyn <maksym.valyn@codeit.pro>
2025-04-17 19:08:52 +0200

    added CACHE_LIFE_PERIOD env variable

--- turbofan.py
+++ turbofan.py
@@ -49,7 +49,7 @@ def generate_temp_file(model, file_format, tessellation=None):
         return tmpfile.name
 
 
-@st.cache_data(ttl=timedelta(hours=0.5), max_entries=10)
+@st.cache_data(ttl=timedelta(seconds=int(os.getenv("CACHE_LIFE_PERIOD"))), max_entries=100)
 def generate_and_export_turbofan_cached(
     _root_curve,
     _middle_curve,
